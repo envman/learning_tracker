3664bea4fc0f9e3ee557e35be6960afd0a826086
Robert Gill <envman@gmail.com>
2021-09-25 16:08:26 +0100

    add buildspec

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,18 +1,23 @@
-import { Repository } from '@aws-cdk/aws-codecommit';
 import * as cdk from '@aws-cdk/core';
 import { CfnOutput } from '@aws-cdk/core';
+import { Repository } from '@aws-cdk/aws-codecommit';
+import { Project, Source } from '@aws-cdk/aws-codebuild';
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const repo = new Repository(this, 'Repository', {
+    const repository = new Repository(this, 'Repository', {
       repositoryName: 'learning-tracker',
       description: 'Demo for AWS things, track and help people learn.',
     });
 
     new CfnOutput(this, 'CLONE_URL', {
-      value: repo.repositoryCloneUrlSsh
+      value: repository.repositoryCloneUrlSsh
+    })
+
+    const project = new Project(this, 'master-build', {
+      source: Source.codeCommit({ repository }),
     })
   }
 }
