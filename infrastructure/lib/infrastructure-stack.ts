24257ce471f889f588c3168d9328a0d645633403
Robert Gill <envman@gmail.com>
2021-09-25 16:36:14 +0100

    add oncommit

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -2,6 +2,8 @@ import * as cdk from '@aws-cdk/core';
 import { CfnOutput } from '@aws-cdk/core';
 import { Repository } from '@aws-cdk/aws-codecommit';
 import { Project, Source } from '@aws-cdk/aws-codebuild';
+import { Pipeline } from '@aws-cdk/aws-codepipeline';
+import { CodeBuildProject } from '@aws-cdk/aws-events-targets';
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -18,7 +20,29 @@ export class InfrastructureStack extends cdk.Stack {
 
     const project = new Project(this, 'master-build', {
       projectName: 'master-build',
-      source: Source.codeCommit({ repository, branchOrRef: 'refs/heads/master' }),
+      source: Source.codeCommit({
+        repository,
+        branchOrRef: 'refs/heads/master'
+      }),
+      environment: {
+        privileged: true
+      },
+      // role:
     })
+
+    repository.onCommit('commitToMaster', {
+      target: new CodeBuildProject(project),
+      branches: ['master']
+    })
+
+    // const pipeline = new Pipeline(this, 'master-build-pipeline', {
+    //   pipelineName: 'master-build-pipeline',
+    //   stages: [
+    //     {
+    //       stageName: '',
+
+    //     }
+    //   ]
+    // })
   }
 }
