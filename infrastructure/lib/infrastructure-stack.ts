72bd444c5a2291744afbdbbaef8b13179ba1d1d4
Robert Gill <envman@gmail.com>
2021-09-26 09:01:32 +0100

    clean up

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,22 +1,9 @@
 import * as cdk from '@aws-cdk/core';
-import { CfnOutput } from '@aws-cdk/core';
-import { Repository } from '@aws-cdk/aws-codecommit';
 import { BuildSpec, EventAction, FilterGroup, LinuxBuildImage, Project, Source } from '@aws-cdk/aws-codebuild';
-import { Pipeline } from '@aws-cdk/aws-codepipeline';
-import { CodeBuildProject } from '@aws-cdk/aws-events-targets';
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // const repository = new Repository(this, 'Repository', {
-    //   repositoryName: 'learning-tracker',
-    //   description: 'Demo for AWS things, track and help people learn.',
-    // });
-
-    // new CfnOutput(this, 'CLONE_URL', {
-    //   value: repository.repositoryCloneUrlSsh
-    // })
 
     const project = new Project(this, 'master-build', {
       projectName: 'learning-tracker',
@@ -32,7 +19,8 @@ export class InfrastructureStack extends cdk.Stack {
         ]
       }),
       environment: {
-        privileged: true
+        privileged: true,
+        buildImage: LinuxBuildImage.STANDARD_5_0
       },
       // role:
     })
@@ -55,42 +43,5 @@ export class InfrastructureStack extends cdk.Stack {
       },
       // role:
     })
-
-    // repository.onCommit('commitToMaster', {
-    //   target: new CodeBuildProject(project),
-    //   branches: ['master']
-    // })
-
-
-
-    // repository.onCommit('prCommit', {
-    //   target: new CodeBuildProject(prProject),
-    //   branches: ['refs/heads/feature/*'],
-    //   eventPattern: {
-
-    //   }
-    // })
-
-    // console.log('test')
-
-    // repository.onPullRequestStateChange('pullRequestStateChange', {
-    //   target: new CodeBuildProject(prProject),
-    //   // eventPattern: {
-    //     // detail: [
-    //       // 'pullRequestCreated',
-    //       // 'pullRequestSourceBranchUpdated'
-    //     // ]
-    //   // }
-    // })
-
-    // const pipeline = new Pipeline(this, 'master-build-pipeline', {
-    //   pipelineName: 'master-build-pipeline',
-    //   stages: [
-    //     {
-    //       stageName: '',
-
-    //     }
-    //   ]
-    // })
   }
 }
